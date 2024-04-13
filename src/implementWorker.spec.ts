2ca7d12ce44f1e474ffb70166a779867cca8665a
Andreas huber <andreashuber69@users.noreply.github.com>
2024-04-13 13:32:00 +0200

    test: error conditions

--- src/implementWorker.spec.ts
+++ src/implementWorker.spec.ts
@@ -3,23 +3,30 @@ import { describe, expect, it } from "vitest";
 import { FibonacciWorker } from "./FibonacciWorker.js";
 import { ThrowingWorker } from "./ThrowingWorker.js";
 
-describe("implementWorker", () => {
-    it("should correctly implement a non-throwing worker", async () => {
+const terminatedError = new Error("The worker has been terminated.");
+
+describe("FibonacciWorker", () => {
+    it("should sequentially execute all calls", async () => {
         const worker = new FibonacciWorker();
         const results = await Promise.all([...new Array(10).keys()].map(async (n) => await worker.execute(n)));
         const expected = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34];
         expect(results.every((value, index) => value === expected[index])).toBe(true);
     });
 
-    it("should correctly implement a throwing worker", async () => {
-        const worker = new ThrowingWorker();
+    it("should throw after terminate()", async () => {
+        const worker = new FibonacciWorker();
+        worker.terminate();
+        worker.terminate(); // Should be safe to call multiple times
+        await expect(async () => await worker.execute(3)).rejects.toThrow(terminatedError);
+    });
+});
 
-        try {
-            await worker.execute();
-            expect(false).toBe(true);
-        } catch (error: unknown) {
-            expect(error).toBeInstanceOf(Error);
-            expect((error as Error).message === "Hmmm").toBe(true);
-        }
+describe("ThrowingWorker", () => {
+    it("should throw for every call", async () => {
+        const worker = new ThrowingWorker();
+        const results = await Promise.allSettled([...new Array(3).keys()].map(async () => await worker.execute()));
+        expect(results.every(({ status }) => status === "rejected")).toBe(true);
+        worker.terminate();
+        await expect(async () => await worker.execute()).rejects.toThrow(terminatedError);
     });
 });
