c60a0b8b22ea2b23adddd596b7af5c5c2d51a3d1
Andreas huber <andreashuber69@users.noreply.github.com>
2024-04-15 16:26:04 +0200

    refactor: add worker file names

--- src/testHelpers/FibonacciWorker.ts
+++ src/testHelpers/FibonacciWorker.ts
@@ -4,4 +4,4 @@ import { implementWorker } from "../implementWorker.js";
 const getFibonacci = (n: number): number => ((n < 2) ? Math.floor(n) : getFibonacci(n - 1) + getFibonacci(n - 2));
 
 export const FibonacciWorker =
-    implementWorker(() => new Worker(new URL(import.meta.url), { type: "module" }), getFibonacci);
+    implementWorker(() => new Worker(new URL("FibonacciWorker.js", import.meta.url), { type: "module" }), getFibonacci);
