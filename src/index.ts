3d8eff88cf916a702055fc65300fbcde1131959e
Andreas huber <andreashuber69@users.noreply.github.com>
2024-04-17 16:17:23 +0200

    feat: implementWorkerExternal to exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 // https://github.com/andreashuber69/kiss-worker/blob/develop/README.md
 export { implementWorker } from "./implementWorker.js";
 
+export { implementWorkerExternal } from "./implementWorkerExternal.js";
+
 export { serve } from "./serve.js";
