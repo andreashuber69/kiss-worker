30b573ef2f7ef01c2df3de9127a171ed9011f707
Andreas huber <andreashuber69@users.noreply.github.com>
2024-04-18 10:33:58 +0200

    docs: add code comments

--- src/testHelpers/FibonacciWorkerExternal.ts
+++ src/testHelpers/FibonacciWorkerExternal.ts
@@ -1,7 +1,11 @@
 // https://github.com/andreashuber69/kiss-worker/blob/develop/README.md
 import { implementWorkerExternal } from "../implementWorkerExternal.js";
+
+// Import the type of our worker function
 import type { getFibonacci } from "./getFibonacci.js";
 
+// Implement and export our worker, passing the type of the worker function
 export const FibonacciWorker = implementWorkerExternal<typeof getFibonacci>(
+    // A function that creates a web worker running the worker script
     () => new Worker(new URL("getFibonacci.js", import.meta.url), { type: "module" }),
 );
