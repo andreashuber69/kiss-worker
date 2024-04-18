a58b5209773b4d7044da1e6721466bc7552d396a
Andreas huber <andreashuber69@users.noreply.github.com>
2024-04-18 09:56:28 +0200

    docs: reduce reference to the public interface

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 // https://github.com/andreashuber69/kiss-worker/blob/develop/README.md
+
+export type { KissWorker } from "./KissWorker.js";
+
+export type { DedicatedWorker } from "./DedicatedWorker.js";
+
 export { implementWorker } from "./implementWorker.js";
 
 export { implementWorkerExternal } from "./implementWorkerExternal.js";
