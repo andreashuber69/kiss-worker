0f441c6ff6d087ae34b66b620076a0b5bbf45bee
Andreas huber <andreashuber69@users.noreply.github.com>
2024-04-21 20:52:27 +0200

    test: revert to testing with .ts files

--- src/testHelpers/FibonacciWorker.d.ts
+++
@@ -1,4 +0,0 @@
-// https://github.com/andreashuber69/kiss-worker/blob/develop/README.md
-import type { KissWorker } from "../KissWorker.ts";
-
-export declare const FibonacciWorker: new () => KissWorker<(n: number) => number>;
---
+++ src/testHelpers/FibonacciWorker.ts
@@ -0,0 +1,9 @@
+// https://github.com/andreashuber69/kiss-worker/blob/develop/README.md
+import { implementWorker } from "../implementWorker.js";
+
+const getFibonacci = (n: number): number => ((n < 2) ? Math.floor(n) : getFibonacci(n - 1) + getFibonacci(n - 2));
+
+export const FibonacciWorker = implementWorker(
+    () => new Worker(new URL("FibonacciWorker.js", import.meta.url), { type: "module" }),
+    getFibonacci,
+);
