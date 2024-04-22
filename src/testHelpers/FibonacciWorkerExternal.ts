20d362fa12af8b2761ea06baf2312123b0fda17b
Andreas huber <andreashuber69@users.noreply.github.com>
2024-04-22 12:54:59 +0200

    chore: remove no longer necessary comments

--- src/testHelpers/FibonacciWorkerExternal.ts
+++ src/testHelpers/FibonacciWorkerExternal.ts
@@ -1,11 +1,7 @@
 // https://github.com/andreashuber69/kiss-worker/blob/develop/README.md
 import { implementWorkerExternal } from "../implementWorkerExternal.js";
-
-// Import the type of our worker function
 import type { GetFibonacci } from "./getFibonacci.js";
 
-// Implement and export our worker, passing the type of the worker function
 export const FibonacciWorker = implementWorkerExternal<GetFibonacci>(
-    // A function that creates a web worker running the worker script
     () => new Worker(new URL("getFibonacci.js", import.meta.url), { type: "module" }),
 );
--- src/testHelpers/getFibonacci.ts
+++ src/testHelpers/getFibonacci.ts
@@ -1,12 +1,9 @@
 // https://github.com/andreashuber69/kiss-worker/blob/develop/README.md
 import { serve } from "../serve.js";
 
-// The function we would like to execute on the worker thread (the worker function)
 const getFibonacci =
     (n: number): number => ((n < 2) ? Math.floor(n) : getFibonacci(n - 1) + getFibonacci(n - 2));
 
-// Serve the function, so that it can be called from another thread
 serve(getFibonacci);
 
-// Export the function type, so that it can be passed to implementWorkerExternal
 export type GetFibonacci = typeof getFibonacci;
