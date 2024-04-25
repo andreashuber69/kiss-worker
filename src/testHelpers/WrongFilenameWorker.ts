80d1cdbdcc9fa1f8591667532f28c25e9b35971d
Andreas huber <andreashuber69@users.noreply.github.com>
2024-04-25 15:15:48 +0200

    test: simplify

--- src/testHelpers/WrongFilenameWorker.ts
+++ src/testHelpers/WrongFilenameWorker.ts
@@ -1,7 +1,5 @@
 // https://github.com/andreashuber69/kiss-worker/blob/develop/README.md
 import { implementWorker } from "../implementWorker.js";
 
-const doNothing = () => undefined;
-
 export const WrongFilenameWorker =
-    implementWorker(() => new Worker(new URL("Oops.js", import.meta.url), { type: "module" }), doNothing);
+    implementWorker(() => new Worker(new URL("Oops.js", import.meta.url), { type: "module" }), () => undefined);
