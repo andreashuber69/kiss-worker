f32a8004a8259def1c18526d5d42a633b93891c3
Andreas huber <andreashuber69@users.noreply.github.com>
2024-04-30 15:18:36 +0200

    test: add implementObjectWorkerExternal.spec.ts

---
+++ src/implementObjectWorkerExternal.spec.ts
@@ -0,0 +1,14 @@
+// https://github.com/andreashuber69/kiss-worker/blob/develop/README.md
+import { describe, expect, it } from "vitest";
+import { CalculatorWorker } from "./testHelpers/CalculatorWorkerExternal.js";
+
+describe("ObjectWorker", () => {
+    describe("obj", () => {
+        it("should offer async variants of the original", async () => {
+            const worker = new CalculatorWorker();
+            expect(await worker.obj.add(1, 2)).toBe(3);
+            expect(await worker.obj.subtract(7, 4)).toBe(3);
+            expect(await worker.obj.format(17)).toBe("17");
+        });
+    });
+});
---
+++ src/testHelpers/Calculator.ts
@@ -0,0 +1,21 @@
+// https://github.com/andreashuber69/kiss-worker/blob/develop/README.md
+import { getObjectDescriptor } from "../getObjectDescriptor.js";
+import { serveObject } from "../serveObject.js";
+
+class Calculator {
+    public add(left: number, right: number) {
+        return left + right;
+    }
+
+    public subtract(left: number, right: number) {
+        return left - right;
+    }
+
+    public async format(num: number) {
+        return await Promise.resolve(`${num}`);
+    }
+}
+
+serveObject(Calculator);
+
+export const calculatorDescriptor = getObjectDescriptor(Calculator);
