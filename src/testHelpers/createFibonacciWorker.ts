da4a4c649b508ee9d9f21bec3da4f0b878ca00d6
Andreas huber <andreashuber69@users.noreply.github.com>
2024-05-04 12:00:30 +0200

    chore: adapt test to example

---
+++ src/testHelpers/createFibonacciWorker.ts
@@ -0,0 +1,15 @@
+// https://github.com/andreashuber69/kiss-worker/blob/develop/README.md
+import { implementFunctionWorker } from "../implementFunctionWorker.js";
+
+// The function we want to execute on a worker thread
+const fibonacci = (n: number): number =>
+    ((n < 2) ? Math.floor(n) : fibonacci(n - 1) + fibonacci(n - 2));
+
+export const createFibonacciWorker = implementFunctionWorker(
+    // A function that creates a web worker running this script
+    () => new Worker(
+        new URL("createFibonacciWorker.js", import.meta.url),
+        { type: "module" },
+    ),
+    fibonacci,
+);
