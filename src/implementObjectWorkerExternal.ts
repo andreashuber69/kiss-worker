ff1b440cad2556daee7b10163bb1d0659a416498
Andreas huber <andreashuber69@users.noreply.github.com>
2024-05-05 12:11:16 +0200

    docs: reword

--- src/implementObjectWorkerExternal.ts
+++ src/implementObjectWorkerExternal.ts
@@ -12,11 +12,11 @@ import type { serveObject } from "./serveObject.js";
  * @description Compared to {@linkcode implementObjectWorker}, {@linkcode implementObjectWorkerExternal} covers the
  * following additional requirements:
  * - The function returned by {@linkcode implementObjectWorkerExternal} can be executed on **any** thread.
- * - The code of the served object is only ever loaded on the worker thread. This can become important when the amount
- * of code of the served object is significant, such that you'd rather not load it on the thread calling
- * {@linkcode ObjectWorker.obj}. Build tools like [vite](vitejs.dev) support this use case by detecting
- * `new Worker(...)` calls and putting the worker script as well as all directly and indirectly called code into a
- * separate chunk. Please see [this example](https://github.com/andreashuber69/kiss-worker-demo4) for more information.
+ * - The code of the served object is only ever loaded on the worker thread. This can become important when the
+ * amount of code running on the worker thread is significant, such that you'd rather not load it anywhere else. Build
+ * tools like [vite](vitejs.dev) support this use case by detecting `new Worker(...)` calls and putting the worker
+ * script as well as all directly and indirectly called code into a separate chunk. Please see
+ * [this example](https://github.com/andreashuber69/kiss-worker-demo4) for more information.
  * @param createWorker A function that creates a new [`Worker`](https://developer.mozilla.org/en-US/docs/Web/API/Worker)
  * with every call. This function **must** create a worker running a script different from the one it is created in.
  * The script must call {@linkcode serveObject} passing a constructor function and export the type of the object.
