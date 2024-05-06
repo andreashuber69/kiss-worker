675740cb1d1b821fccba87ac83f60809fd7ca2e4
Andreas huber <andreashuber69@users.noreply.github.com>
2024-05-06 12:03:23 +0200

    refactor: rename

--- src/testHelpers/createCalculatorWorkerExternal.ts
+++ src/testHelpers/createCalculatorWorkerExternal.ts
@@ -1,9 +1,9 @@
 // https://github.com/andreashuber69/kiss-worker/blob/develop/README.md
 import { implementObjectWorkerExternal } from "../implementObjectWorkerExternal.js";
 import { ObjectInfo } from "../ObjectInfo.js";
-import type { CalculatorExternal } from "./CalculatorExternal.js";
+import type { Calculator } from "./Calculator.js";
 
 export const createCalculatorWorkerExternal = implementObjectWorkerExternal(
-    () => new Worker(new URL("CalculatorExternal.js", import.meta.url), { type: "module" }),
-    new ObjectInfo<typeof CalculatorExternal>("addExternal", "subtractExternal", "formatExternal"),
+    () => new Worker(new URL("Calculator.js", import.meta.url), { type: "module" }),
+    new ObjectInfo<typeof Calculator>("add", "subtract"),
 );
