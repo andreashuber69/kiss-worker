ba44a339d1d33e8af18d3e30988db0af8479fb4a
Andreas huber <andreashuber69@users.noreply.github.com>
2024-05-11 12:29:08 +0200

    refactor: simplify

--- src/testHelpers/createUniversalFunctionWorker.ts
+++ src/testHelpers/createUniversalFunctionWorker.ts
@@ -1,9 +1,7 @@
 // https://github.com/andreashuber69/kiss-worker/blob/develop/README.md
 import { implementFunctionWorker } from "../implementFunctionWorker.js";
 
-const callFunction = (fun: () => unknown) => fun();
-
 export const createUniversalFunctionWorker = implementFunctionWorker(
     () => new Worker(new URL("createUniversalFunctionWorker.js", import.meta.url), { type: "module" }),
-    callFunction,
+    (fun: () => unknown) => fun(),
 );
