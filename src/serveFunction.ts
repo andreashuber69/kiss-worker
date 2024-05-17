bfaf1f499abf12ce49f0d3a111a5b04fd4bdf46d
Andreas huber <andreashuber69@users.noreply.github.com>
2024-05-17 12:58:05 +0200

    refactor: extract handlers into separate functions

--- src/serveFunction.ts
+++ src/serveFunction.ts
@@ -1,6 +1,20 @@
 // https://github.com/andreashuber69/kiss-worker/blob/develop/README.md
 import type { implementFunctionWorkerExternal } from "./implementFunctionWorkerExternal.js";
 
+// Code coverage is not reported for code executed within a worker, because only the original (uninstrumented)
+// version of the code is ever loaded.
+/* istanbul ignore next -- @preserve */
+const handleMessage = async <T extends (...args: never[]) => unknown>(func: T, ev: MessageEvent<Parameters<T>>) => {
+    try {
+        // We cannot know whether func is synchronous or asynchronous, we therefore must always await
+        // the result.
+        postMessage({ type: "result", result: await func(...ev.data) });
+    } catch (error: unknown) {
+        postMessage({ type: "error", error });
+    }
+};
+
+/* istanbul ignore next -- @preserve */
 /**
  * Serves `func` on a worker thread such that it can be called from the thread calling
  * {@linkcode implementFunctionWorkerExternal}.
@@ -9,16 +23,5 @@ import type { implementFunctionWorkerExternal } from "./implementFunctionWorkerE
  * @param func The function to serve.
  */
 export const serveFunction = <T extends (...args: never[]) => unknown>(func: T) => {
-    // Code coverage is not reported for code executed within a worker, because only the original (uninstrumented)
-    // version of the code is ever loaded.
-    /* istanbul ignore next -- @preserve */
-    onmessage = async (ev: MessageEvent<Parameters<T>>) => {
-        try {
-            // We cannot know whether func is synchronous or asynchronous, we therefore must always await
-            // the result.
-            postMessage({ type: "result", result: await func(...ev.data) });
-        } catch (error: unknown) {
-            postMessage({ type: "error", error });
-        }
-    };
+    onmessage = (ev: MessageEvent<Parameters<T>>) => void handleMessage(func, ev);
 };
--- src/serveObject.ts
+++ src/serveObject.ts
@@ -4,6 +4,35 @@ import type { implementObjectWorkerExternal } from "./implementObjectWorkerExter
 import type { MethodsOnlyObject } from "./MethodsOnlyObject.js";
 import type { WorkerSignature } from "./Signature.js";
 
+// Code coverage is not reported for code executed within a worker, because only the original (uninstrumented)
+// version of the code is ever loaded.
+/* istanbul ignore next -- @preserve */
+const handleMessage = async <C extends new (...args: never[]) => T, T extends MethodsOnlyObject<T> = InstanceType<C>>(
+    ctor: C,
+    obj: T | undefined,
+    { data }: MessageEvent<Parameters<WorkerSignature<C, T>>>,
+) => {
+    try {
+        if (data[0] === "construct") {
+            const [, ...args2] = data;
+            const newObj = new ctor(...args2);
+            postMessage({ type: "result", result: getAllPropertyNames(newObj) });
+            return newObj;
+        }
+
+        const [, methodName, ...args] = data;
+
+        // We cannot know whether the called method is synchronous or asynchronous, we therefore must always
+        // await the result.
+        postMessage({ type: "result", result: await obj?.[methodName](...args) });
+        return obj;
+    } catch (error: unknown) {
+        postMessage({ type: "error", error });
+        return obj;
+    }
+};
+
+/* istanbul ignore next -- @preserve */
 /**
  * Calls `ctor` and serves the returned object on a worker thread such that its methods can be called from
  * the thread calling {@linkcode implementObjectWorkerExternal}.
@@ -14,27 +43,13 @@ import type { WorkerSignature } from "./Signature.js";
 export const serveObject = <C extends new (...args: never[]) => T, T extends MethodsOnlyObject<T> = InstanceType<C>>(
     ctor: C,
 ) => {
-    // Code coverage is not reported for code executed within a worker, because only the original (uninstrumented)
-    // version of the code is ever loaded.
-    /* istanbul ignore next -- @preserve */
     let obj: T | undefined;
 
-    /* istanbul ignore next -- @preserve */
-    onmessage = async ({ data }: MessageEvent<Parameters<WorkerSignature<C, T>>>) => {
-        try {
-            if (data[0] === "construct") {
-                const [, ...args] = data;
-                obj = new ctor(...args);
-                postMessage({ type: "result", result: getAllPropertyNames(obj) });
-            } else {
-                const [, methodName, ...args] = data;
+    const handleMessageWithObject =
+        // This is not a race condition because the the obj variable changes its value only exactly once and sensible
+        // client code has no way of triggering new messages before the first message has been fully processed.
+        // eslint-disable-next-line require-atomic-updates
+        async (ev: MessageEvent<Parameters<WorkerSignature<C, T>>>) => (obj = await handleMessage(ctor, obj, ev));
 
-                // We cannot know whether the called method is synchronous or asynchronous, we therefore must always
-                // await the result.
-                postMessage({ type: "result", result: await obj?.[methodName](...args) });
-            }
-        } catch (error: unknown) {
-            postMessage({ type: "error", error });
-        }
-    };
+    onmessage = (ev: MessageEvent<Parameters<WorkerSignature<C, T>>>) => void handleMessageWithObject(ev);
 };
