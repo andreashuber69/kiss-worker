4aaed9265c731eb728ae0bb7043476dda7abb900
Andreas huber <andreashuber69@users.noreply.github.com>
2024-05-27 10:47:09 +0200

    test: minor change

--- src/testHelpers/createFunnyWorker.ts
+++ src/testHelpers/createFunnyWorker.ts
@@ -6,13 +6,13 @@ import { postMessage } from "api";
 const doFunnyThings = async (what: "post" | "throw" | "throwDelayed" | "throwOutside") => {
     switch (what) {
         case "post":
-            postMessage("It's sunny outside");
+            postMessage("It's sunny outside.");
             break;
         case "throw":
             throw new Error("Hmmm");
         case "throwOutside":
             setTimeout(() => {
-                throw new Error("It's sunny outside");
+                throw new Error("It's sunny outside.");
             });
 
             await new Promise((resolve) => setTimeout(resolve, 100));
